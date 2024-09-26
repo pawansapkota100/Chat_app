e1ed8140b1a4a743eacbbf9cf8acbc4c6c2f2834
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-09-26 11:45:44 +0545

    initial commit

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def room(request, room_name):
+    context={"room_name":room_name}
+    return render(request, 'core/room.html',context )
