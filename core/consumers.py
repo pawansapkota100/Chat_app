59bfeaddbc98934c049f347b11124bb462449520
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-09-27 14:56:39 +0545

    fix: chatapp combining db

--- core/consumers.py
+++ core/consumers.py
@@ -1,38 +1,53 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from .models import Chatroom, Message
+from channels.db import database_sync_to_async
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    
+    async def connect(self):
+        self.room_name= self.scope['url_route']['kwargs']['room_name']
+        self.group_name= f"chat_{self.room_name}"
 
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
+        await self.channel_layer.group_add( self.group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self):
+
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+    
+    @database_sync_to_async
+    def get_chatroom(self, room_name):
+        return Chatroom.objects.get(name= self.room_name)
+    
+    @database_sync_to_async
+    def create_message(self, room, user, content):
+        return Message.objects.create(room= room, content= content)
+
+    async def receive(self, text_data):
+        text_data_json= json.loads(text_data)
+        message= text_data_json['message']
+        user= self.scope["user"]
+
+        # room= await self.get_chatroom(self.room_name)
+
+        # await self.create_message(room,user,content= message)
+        # await Message.objects.create(room=room, user=user, content= message)
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'chat_message',
+
+                'message': message,
+                'user': user.username
+            }
         )
+    async def chat_message(self, event):
+        Message= event['message']
+        user= event['user']
 
-        self.accept()
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
-
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
-        )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event["message"]
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({
+            'message': Message, 
+            'user': user
+        }))
--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model  
 
+user= get_user_model()
 # Create your models here.
+
+
+class Chatroom(models.Model):
+    name= models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    room= models.ForeignKey(Chatroom, on_delete=models.CASCADE, related_name='messages')
+    user= models.ForeignKey(user, on_delete=models.CASCADE, related_name='messages')
+    content= models.TextField()
+    timestamp= models.DateTimeField(auto_now_add=True)      
+
+    class Meta:
+        ordering= ('timestamp',)
+    
+    def __str__(self):
+        return f'{self.sender}: {self.content[:50]}'
